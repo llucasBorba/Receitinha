5cfecff0a56ad921d47bc8f9536ffbbdc98e0d89
llucasBorba <lucasborba6075@gmail.com>
2024-11-19 15:30:59 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style ={styles.date}>
+        <Text style={styles.hoje}>Hoje, </Text> <Text>1 fev 2020</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +14,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ECECEC',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  date: { 
+    backgroundColor: "white", 
+    width: 300,
+    height: 100,
+    flexDirection: "row"
+  }, 
+  hoje: {
+    color: "00664E",
+    fontWeight: "800"
+    
+  }
 });
