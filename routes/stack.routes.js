e6b54f5a368d0be3a8feff5c81461e2ac052fb68
llucasBorba <lucasborba6075@gmail.com>
2024-11-21 10:59:51 -0300

    melhorado

--- routes/stack.routes.js
+++ routes/stack.routes.js
@@ -3,15 +3,32 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Teste from '../screens/Teste';
 import Home from '../screens/Home';
+import { HeaderTitle } from '@react-navigation/elements';
 
 const Stack = createStackNavigator();
 
 export default function StackRoute() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Teste" component={Teste} />
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName="Home" >
+
+        <Stack.Screen 
+        name="Home" 
+        component={Home}
+        options={{headerShown: false}}
+        />
+
+        <Stack.Screen 
+        name="Teste" 
+        component={Teste}
+         options={{
+          title: false,
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: "#f5f5f5",
+          }
+           }}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
