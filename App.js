7d499527c4ff8517e2b4866f9207671f5e082322
GabrielBorgess <staffgabs@gmail.com>
2024-11-21 19:20:29 -0300

    adding font config

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import StackRoute from './routes/stack.routes';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [fontsLoaded, error] = useFonts({
+    'New-york': require('./assets/fonts/NewYork.otf'),
+  });
+
+  useEffect(()=>{
+    if(fontsLoaded || error){
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error])
+
+  if(!fontsLoaded && !error) return null;  
+
   return <StackRoute/>;
 }
